8ca65eb97f02a72267faf0a5d3c880c566599496
Abdul Muizz Fizal <abdulmuizzf@gmail.com>
2020-12-05 23:12:45 +0530

    Write basic views and render a template manually

--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.template import loader
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("HelloWorld! You're at the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("You're viewing question %s" % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You voted for question %s" % question_id)
